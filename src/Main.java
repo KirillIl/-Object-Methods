9a033b1e8e8c1cd53fafcdbc2f83160d3c2670d3
Admin <ilinskiy_kirill@mail.ru>
2022-10-30 22:05:33 +0300

    готово на проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book book1 = new Book("It", new Author("Stephen", "King"), 1986 );
+        System.out.println(book1);
+
+
+        Book book2 = new Book("Alice's Adventures in Wonderland", new Author("Lewis", "Carroll"), 1865);
+        System.out.println(book2);
+        System.out.println(book1.equals(book2));
+        System.out.println(book1.hashCode());
+        System.out.println(book2.hashCode());
     }
 }
